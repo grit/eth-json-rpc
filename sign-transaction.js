44f9e66025d06f020cda74aa5bc40aa3b60e4658
proxyGrit <dev@proxygrit.com>
2022-05-16 00:54:40 -0700

    Modify JSON-RPC request to interact with contract function

--- sign-transaction.js
+++ sign-transaction.js
@@ -8,38 +8,50 @@ const common = new Common({ chain: Chain.Goerli, hardfork: Hardfork.London });
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ALCHEMY_ENDPOINT = process.env.ALCHEMY_ENDPOINT;
 
-const txParams = {
-  nonce: '0x0', // manually change
-  gasLimit: '0x5208',
-  maxPriorityFeePerGas: '0x3b9aca00',
-  maxFeePerGas: '0x3b9acaff',
-  to: '0x179F15f55a734f3001325eeA4F0D0d6CbA5e1657',
-  value: '0x5af3107a4000', // .0001 ether
-  chainId: '0x05', // goerli chain id
-  type: '0x02', // eip 1559
-};
+(async () => {
+  const txParams = {
+    nonce: '0x2', // manually change
+    // gasLimit: '0x5208',
+    maxPriorityFeePerGas: '0x3b9aca00',
+    maxFeePerGas: '0x3b9acaff',
+    to: '0xb7a0706d669eee9eaf9d0e6d9cdf075c31a19f6a',
+    value: '0x0',
+    data: '0xd09de08a',
+    // chainId: '0x05', // goerli chain id
+    type: '0x02', // eip 1559
+  };
 
-const tx = FeeMarketEIP1559Transaction.fromTxData(txParams, { common });
-
-const privateKey = Buffer.from(PRIVATE_KEY, 'hex');
-
-const signedTx = tx.sign(privateKey);
-
-const serializedTx = signedTx.serialize();
-
-const rawTx = '0x' + serializedTx.toString('hex');
-
-axios
-  .post(ALCHEMY_ENDPOINT, {
+  const response = await axios.post(ALCHEMY_ENDPOINT, {
     jsonrpc: '2.0',
     id: 1,
-    method: 'eth_sendRawTransaction',
-    params: [rawTx],
-  })
-  .then(response => {
-    if (response.data.error) {
-      console.log(response.data.error);
-    } else {
-      console.log('Tx hash: ' + response.data.result);
-    }
+    method: 'eth_estimateGas',
+    params: [txParams],
   });
+
+  txParams.gasLimit = response.data.result;
+
+  const tx = FeeMarketEIP1559Transaction.fromTxData(txParams, { common });
+
+  const privateKey = Buffer.from(PRIVATE_KEY, 'hex');
+
+  const signedTx = tx.sign(privateKey);
+
+  const serializedTx = signedTx.serialize();
+
+  const rawTx = '0x' + serializedTx.toString('hex');
+
+  axios
+    .post(ALCHEMY_ENDPOINT, {
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'eth_sendRawTransaction',
+      params: [rawTx],
+    })
+    .then(response => {
+      if (response.data.error) {
+        console.log(response.data.error);
+      } else {
+        console.log('Tx hash: ' + response.data.result);
+      }
+    });
+})();
